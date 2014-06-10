992758f4bf2831bc1dbf582e3529e772ac90abb0
Alexey Verein <tpom6oh@yandex-team.ru>
2014-06-10 11:18:05 +0400

    Add actionbar

--- src/com/tpom6oh/employees/MainActivity.java
+++ src/com/tpom6oh/employees/MainActivity.java
@@ -1,9 +1,15 @@
 package com.tpom6oh.employees;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends ActionBarActivity {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +17,23 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
